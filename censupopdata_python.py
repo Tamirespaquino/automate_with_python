63f64fcd14e1f9fd21ffc562ef196e346c2268b6
Tamires Pedrali de Aquino <tamirespaquino@gmail.com>
2020-10-28 20:34:28 -0300

    Finalização da automação da planilha

--- censupopdata_python.py
+++ censupopdata_python.py
@@ -26,3 +26,24 @@ countyData[state][county]['pop'] += int(pop)
 #Para ter certeza que a abreviação da cidade, usada na planilha Excel, será reconhecida pelo código, 
 #usamos o método setdefault() para marcar um valor, caso algum ainda não existe para "state".
 
+#Depois que o "for" terminar, o dicionário countyData conterá toda população e as informações de cada
+#cidade e setor. Usaremos, então, a função pprint.pformat pra escrever como string, num arquivo chamado 
+#census2010.py, os valores do countyData. 
+
+print('Writing results')
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done.')
+
+#A função pprint produz uma string já validada pelo python. Então, gerou-se um arquivo python deste arquivo python,
+#sendo o primrio denominado census2010.py. 
+#Agora, vamos mudar o diretório de trabalho para o arquivo census2010.py e importá-lo.
+
+import os
+import census2010
+census2010.allData['WY']['Weston']
+anchoragePop = census2010.allData['WY']['Weston']['pop']
+print('The 2010 population of Weston was ' + str(anchoragePop))
+
+
